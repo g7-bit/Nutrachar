0de19c1b028d883253574bc79ea4cee1fcaeaf59
g7-bit <65290200+g7-bit@users.noreply.github.com>
2025-07-08 13:04:22 +0530

    implemented pie logic, added reference pie

--- frontend/src/components/Chart.jsx
+++ frontend/src/components/Chart.jsx
@@ -1,47 +1,55 @@
-import React from 'react';
-import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip, LabelList, Label, Legend } from 'recharts';
+import React from "react";
+import {
+  PieChart,
+  Pie,
+  Cell,
+  ResponsiveContainer,
+  Tooltip,
+  LabelList,
+  Label,
+  Legend,
+} from "recharts";
 
-  // const data = [
-  //   { name: 'Protein', value: 125 },
-  //   { name: 'Fats', value: 323 },
-  //   { name: 'Carbs', value: 335 }
-  // ];
+// const data = [
+//   { name: 'Protein', value: 125 },
+//   { name: 'Fats', value: 323 },
+//   { name: 'Carbs', value: 335 }
+// ];
 
-  const COLORS = ['#0088FE', '#00C49F', '#FFBB28'];
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28"];
 
-function Chart({data}) {
-  console.log("data arrived in chart.jsx", data)
+function Chart({ data, userLabel = "", height=250 , circleRadius=80, width="100%"}) {
+  // console.log("data arrived in chart.jsx", data);
 
-  const noData = data.reduce((ele, item)=> ele + item.value, 0)
-  if(noData === 0){
-    return (
-      <div> Add some food item to see chart</div>
-    )
+  const noData = data.reduce((ele, item) => ele + item.value, 0);
+  if (noData === 0) {
+    return <div> Add some food item to see chart</div>;
   }
-  console.log("no data? ", noData)
-    return (
-      <ResponsiveContainer width="100%" height={400}>
-        <PieChart>
-          <Pie
-            data={data}
-            cx="50%"
-            cy="50%"
-            outerRadius={80}
-            fill="#8884d8"
-            dataKey="value"
-            label = {({name, value, percent})=> `${name} : ${(percent * 100).toFixed(0)}%`}
-          >
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
-          </Pie>
-          <Tooltip/>
-          <Legend/>
-        </PieChart>
+  // console.log("no data? ", noData);
+  return (
+    <ResponsiveContainer   width={width} height={height}>
+      <PieChart >
+        <Pie
+        
+          data={data}
+          cx="50%"
+          cy="50%"
+          outerRadius={circleRadius}
+          fill="#8884d8"
+          dataKey="value"
+          label={({ name, value, percent }) =>
+            `${name} - ${(percent * 100).toFixed(0)}%`
+          }
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Tooltip />
+      </PieChart>
+      <div className="text-center opacity-100">{userLabel}</div>
+    </ResponsiveContainer>
+  );
+}
 
-      </ResponsiveContainer>
-    );
-  }
-
-
-  export default Chart;
+export default Chart;
--- frontend/src/components/Diet.jsx
+++ frontend/src/components/Diet.jsx
@@ -11,7 +11,7 @@ function Diet() {
   const [multiplierArr, setMultiplierArr] = useState([]);
   const [loading, setLoading] = useState(true);
   const [totalMacros, setTotalMacros] = useState({});
-  const [valForCharts, setValForCharts]= useState([])
+  const [valForCharts, setValForCharts] = useState([]);
 
   const tableHeading = [
     "Food name",
@@ -28,29 +28,29 @@ function Diet() {
   const roundToThree = (num) => Math.round(num * 1000) / 1000;
   // setting Total row
 
+  function calculateData(data) {
+    console.log("'data' inside calculated fn, ", data);
 
-  function calculateData(data){
-    console.log("'data' inside calculated fn, ",data)
-    
-    const updatedArr = data.map((foodObj)=>{
-      
-      const newFoodObj = {...foodObj}
-      Object.keys(newFoodObj).forEach(foodItem=>{
-        if(typeof newFoodObj[foodItem] === 'number' && foodItem !== 'quantity' && foodItem !== '__v'){
+    const updatedArr = data.map((foodObj) => {
+      const newFoodObj = { ...foodObj };
+      Object.keys(newFoodObj).forEach((foodItem) => {
+        if (
+          typeof newFoodObj[foodItem] === "number" &&
+          foodItem !== "quantity" &&
+          foodItem !== "__v"
+        ) {
           // console.log(newFoodObj[foodItem], newFoodObj.quantity, foodItem)
-          newFoodObj[foodItem]= newFoodObj[foodItem] * newFoodObj.quantity
+          newFoodObj[foodItem] =
+            (newFoodObj[foodItem] / 100) * newFoodObj.quantity;
         }
         // console.log(foodObj.quantity)
         // console.log(foodItem)
-      })
+      });
       return newFoodObj;
-    })
+    });
 
-    setCurrentData(updatedArr)
+    setCurrentData(updatedArr);
   }
-
-
-
 
   // setting data for total row
   useEffect(() => {
@@ -74,7 +74,7 @@ function Diet() {
     }, {});
     // console.log("total boj", totalObj);
     setTotalMacros(totalObj);
-    
+
     // const valForCharts = []
 
     // Object.keys(totalObj).forEach(items=>{
@@ -86,16 +86,17 @@ function Diet() {
     //     }
     //     valForCharts.push(obj)
     //   }
-        
+
     // })
 
-      const valForCharts = ['fats', 'carbs', 'protein']
-    .map(key => ({ name: key, value: totalObj[key] }))
+    const valForCharts = ["fats", "carbs", "protein"].map((key) => ({
+      name: key,
+      value: totalObj[key],
+    }));
 
-    setValForCharts(valForCharts)
+    setValForCharts(valForCharts);
 
     // const chartVals = totalObj.
-
   }, [currentData]);
 
   //Handle onChange
@@ -105,27 +106,31 @@ function Diet() {
 
     // console.log("multiplier in handleOnChange", multiplierArr)
 
-    const multiplierObj = multiplierArr.find(obj=>obj.foodName === fName)
-    
-    if(multiplierObj){
-      const updatedFoodObj = currentData.map(foodObj=>{
-        if(foodObj.foodName ===fName){
-          const updatedFoodObj = {...foodObj}
-          updatedFoodObj.quantity = value
+    const multiplierObj = multiplierArr.find((obj) => obj.foodName === fName);
 
-          Object.keys(updatedFoodObj).forEach(foodItem=>{
-            if(typeof updatedFoodObj[foodItem] === 'number' && foodItem !== 'quantity' && foodItem !== '__v'){
-              updatedFoodObj[foodItem] = multiplierObj[foodItem] * value
+    if (multiplierObj) {
+      const updatedFoodObj = currentData.map((foodObj) => {
+        if (foodObj.foodName === fName) {
+          const updatedFoodObj = { ...foodObj };
+          updatedFoodObj.quantity = value;
+
+          Object.keys(updatedFoodObj).forEach((foodItem) => {
+            if (
+              typeof updatedFoodObj[foodItem] === "number" &&
+              foodItem !== "quantity" &&
+              foodItem !== "__v"
+            ) {
+              updatedFoodObj[foodItem] =
+                (multiplierObj[foodItem] / 100) * value;
             }
-          })
-          return updatedFoodObj           
+          });
+          return updatedFoodObj;
         }
-        return foodObj
-      })
+        return foodObj;
+      });
       // console.log("updated handle onsubmit :: ", updatedFoodObj)
-      setCurrentData(updatedFoodObj)
+      setCurrentData(updatedFoodObj);
     }
-
   };
 
   // Main Api call,
@@ -141,10 +146,8 @@ function Diet() {
         // console.log("diet data in else:: isArray?", Array.isArray(data)); //is true here
         console.log("Main :: Api call to backend :: Data:: ", data);
 
-        calculateData(data)
-        setMultiplierArr(data)
-
-
+        calculateData(data);
+        setMultiplierArr(data);
       }
       setLoading(false);
     };
@@ -159,7 +162,7 @@ function Diet() {
   useEffect(() => {}, []);
 
   return (
-    <div>
+    <div className="mb-10">
       <div className="text-red-400 md:text-green-400 ">
         Width COlor sm: red, md: green
       </div>
@@ -192,16 +195,11 @@ function Diet() {
                     .filter((key) => key !== "_id" && key !== "__v")
                     .map((foodItem) =>
                       foodItem === "foodName" ? (
-                        <td
-                          key={foodItem}
-                          className=""
-                        >
+                        <td key={foodItem} className="">
                           {foodObj[foodItem]}
                         </td>
                       ) : foodItem === "quantity" ? (
-                        <td 
-                        className=""
-                        key={foodItem}>
+                        <td className="" key={foodItem}>
                           <Input
                             type="number"
                             value={foodObj[foodItem]}
@@ -230,16 +228,13 @@ function Diet() {
               <tr>
                 {Object.keys(totalMacros).map((macro) =>
                   macro === "foodName" ? (
-                    <td 
-                    className=""
-                    key={macro}>{totalMacros[macro]}</td>
+                    <td className="" key={macro}>
+                      {totalMacros[macro]}
+                    </td>
                   ) : macro === "quantity" ? (
                     <td key={totalMacros[macro]}></td>
                   ) : (
-                    <td
-                      key={macro}
-                      className="text-center text-nowrap"
-                    >
+                    <td key={macro} className="text-center text-nowrap px-2">
                       {roundToThree(totalMacros[macro])}
                     </td>
                   )
@@ -250,14 +245,65 @@ function Diet() {
         </table>
       </div>
       <div>
+        <div className="m-3">
 
-      <Chart
-      
-      data = {valForCharts}
-      />
+        <Chart data={valForCharts} />
+        </div>
+<br />
+<br />
+<br />
+        <div className="">
+          <div className="text-center text-green-500"> Reccomended Ratio</div>
+          <div className="flex  opacity-85">
+            
+             <div className="flex-1  ">
+                <Chart
+                  data={[
+                    { name: "Fats", value: 25 },
+                    { name: "Carbs", value: 30 },
+                    { name: "Protein", value: 45 },
+                  ]}
+                  userLabel="Weight loss"
+                  height={160}
+                  circleRadius={50}                  
+                />
+              </div>
+
+              
+
+              <div className="flex-1 ">
+                <Chart
+                  data={[
+                    { name: "Fats", value: 25 },
+                    { name: "Carbs", value: 50 },
+                    { name: "Protein", value: 25 },
+                  ]}
+                  userLabel="Maintenance"
+                  height={160}
+                  circleRadius={50}
+                  width=""
+                />
+              </div>
+
+
+              <div className="flex-1">
+                
+                <Chart
+                  data={[
+                    { name: "Fats", value: 20 },
+                    { name: "Carbs", value: 45 },
+                    { name: "Protein", value: 35 },
+                  ]}
+                  userLabel="Muscle Gain"
+                  height={160}
+                  circleRadius={50}
+                />
+                  
+              </div>
+
+          </div>
+        </div>
       </div>
-
-
     </div>
   );
 }
