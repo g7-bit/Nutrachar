e8eeb11f3cadc697e3ca09c15506dcc46a53591b
g7-bit <65290200+g7-bit@users.noreply.github.com>
2025-09-15 18:31:29 +0530

    api repair

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,7 +1,7 @@
-import dotenv from 'dotenv'
-dotenv.config({
-    path:'./.env'
-})
+// import dotenv from 'dotenv'
+// dotenv.config({
+//     path:'./.env'
+// })
 
 import {v2 as cloudinary} from "cloudinary"
 import fs  from "fs"
